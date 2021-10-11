9a1b519a6dc0ad277138777e06bab1c642551c38
Muhammad Zafar <>
2021-10-10 22:42:08 -0400

    Week 06 Homework

---
+++ src/main/java/GraphicsContext.java
@@ -0,0 +1,24 @@
+
+import javafx.scene.paint.Color;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author awais
+ */
+class GraphicsContext {
+
+    void fillRect(int i, int i0, int i1, int i2) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    void setFill(Color LIGHTBLUE) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
